218757a2a6eda579613ddc4ca54ea3208e7cca8b
afozbek <furkanozbek1995@gmail.com>
2020-09-05 22:24:28 +0300

    yeni post methodu eklendi

--- controllers/metric.js
+++ controllers/metric.js
@@ -1,26 +1,42 @@
 const Metric = require("../models/metrics");
+const e = require("cors");
 
-exports.postMetric = (req, res, next) => {
+exports.postMetric = async (req, res, next) => {
   const { timestamp, measureValue, measureName } = req.body;
 
-  console.log("receiving data ...");
-  console.log("body is ", req.body);
-  const newMetric = new Metric({
-    timestamp,
-    measureValue,
-    measureName,
-  });
+  try {
+    const existingMetric = await Metric.findOne({ measureName });
 
-  newMetric
-    .save()
-    .then((result) => {
-      console.log(result);
-      res.send(req.body);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(203).send({ message: "Error Happened" });
-    });
+    if (existingMetric) {
+      existingMetric.measureData.timestamps = [
+        ...existingMetric.measureData.timestamps,
+        timestamp,
+      ];
+      existingMetric.measureData.measureTimes = [
+        ...existingMetric.measureData.measureTimes,
+        measureValue,
+      ];
+
+      const result = await existingMetric.save();
+
+      res.status(200).send({ result, message: "Updated Metric" });
+    } else {
+      const newMetric = new Metric({
+        measureName,
+        measureData: {
+          timestamps: [timestamp],
+          measureTimes: [measureValue],
+        },
+      });
+
+      const result = await newMetric.save();
+
+      res.status(203).send({ result, message: "New Metric Created" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ err });
+  }
 };
 
 exports.getMetric = (req, res, next) => {
--- models/metrics.js
+++ models/metrics.js
@@ -2,11 +2,22 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+// const measureData = new Schema(
+//   {
+//     timestamps: { type: Array },
+//     measureTimes: { type: Array },
+//   },
+//   { timestamps: { createdAt: "created_at" } }
+// );
+
+// const MeasureData = mongoose.model("MeasureData", measureData);
 const metricSchema = new Schema(
   {
-    timestamp: { type: Number },
-    measureName: { type: String },
-    measureValue: { type: String },
+    measureName: { type: String, unique: true },
+    measureData: {
+      timestamps: { type: Array },
+      measureTimes: { type: Array },
+    },
   },
   { timestamps: { createdAt: "created_at" } }
 );
