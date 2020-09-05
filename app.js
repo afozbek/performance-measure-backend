15261c03a37f948da76011bb6f9624fdc9341b42
afozbek <furkanozbek1995@gmail.com>
2020-09-05 14:53:16 +0300

    start script leri değiştirildi

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ var indexRouter = require("./routes/index");
 
 var app = express();
 
-var corsOptions = {
-  origin: "http://localhost:3000",
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
-app.use(cors(corsOptions));
+// var corsOptions = {
+//   origin: "http://localhost:3000",
+//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
+app.use(cors());
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
