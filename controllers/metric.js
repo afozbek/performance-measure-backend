cb336127d64c23e82083daaeab23e0c6747849bd
afozbek <furkanozbek1995@gmail.com>
2020-09-07 17:43:09 +0300

    metric controller güncellendi

--- controllers/metric.js
+++ controllers/metric.js
@@ -2,6 +2,41 @@ const Metric = require("../models/metrics");
 
 const moment = require("moment");
 
+exports.getMetric = (req, res, next) => {
+  const currentTimestamp = Date.now();
+  console.log(req.query);
+
+  const newDateTimeStamp = moment(currentTimestamp)
+    .subtract(req.query.lastMinutes || 30, "minutes")
+    .valueOf();
+
+  console.log(newDateTimeStamp);
+
+  Metric.aggregate([
+    {
+      $project: {
+        measureName: 1,
+        _id: 0,
+        measureData: {
+          $filter: {
+            input: "$measureData",
+            as: "item",
+            cond: { $gt: ["$$item.timestamp", newDateTimeStamp] },
+          },
+        },
+      },
+    },
+  ])
+    .then((metrics) => {
+      // metrics: [{},{},{},{}]
+      res.status(200).send(metrics);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send({ message: "Metric getirmede hata oluştu" });
+    });
+};
+
 exports.postMetric = async (req, res, next) => {
   const { timestamp, measureValue, measureName } = req.body;
 
@@ -25,7 +60,7 @@ exports.postMetric = async (req, res, next) => {
 
       await newMetric.save();
 
-      res.status(203).send({ message: "New Metric Created" });
+      res.status(201).send({ message: "New Metric Created" });
     }
   } catch (err) {
     console.log(err);
@@ -69,39 +104,6 @@ exports.getMetric2 = (req, res, next) => {
       });
 
       res.status(200).send(newMetrics);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.send({ message: "Metric getirmede hata oluştu" });
-    });
-};
-
-exports.getMetric = (req, res, next) => {
-  const currentTimestamp = Date.now();
-  const newDateTimeStamp = moment(currentTimestamp)
-    .subtract(30, "minutes")
-    .valueOf();
-
-  console.log(newDateTimeStamp);
-
-  Metric.aggregate([
-    {
-      $project: {
-        measureName: 1,
-        _id: 0,
-        measureData: {
-          $filter: {
-            input: "$measureData",
-            as: "item",
-            cond: { $gt: ["$$item.timestamp", newDateTimeStamp] },
-          },
-        },
-      },
-    },
-  ])
-    .then((metrics) => {
-      // metrics: [{},{},{},{}]
-      res.status(200).send(metrics);
     })
     .catch((err) => {
       console.log(err);
